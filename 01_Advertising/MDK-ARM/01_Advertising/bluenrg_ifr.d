a27f60b814d7366b131a8162f3ba09f8c5e47fc1
pranit-ekatpure <pranitekatpure@gmail.com>
2020-11-10 01:28:33 +0530

    added BLE advertising example project

---
+++ 01_Advertising/MDK-ARM/01_Advertising/bluenrg_ifr.d
@@ -0,0 +1,6 @@
+01_advertising\bluenrg_ifr.o: ../Middlewares/ST/BlueNRG-MS/hci/controller/bluenrg_IFR.c
+01_advertising\bluenrg_ifr.o: ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_types.h
+01_advertising\bluenrg_ifr.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+01_advertising\bluenrg_ifr.o: ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_def.h
+01_advertising\bluenrg_ifr.o: ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_updater_aci.h
+01_advertising\bluenrg_ifr.o: ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_utils.h
